14e53c88712ceff5b4d663506e6f2f328c4aabb2
Matthew Carter <m@ahungry.com>
2018-06-16 23:32:49 -0400

    Update settings

--- packed/player/Player.gd
+++ packed/player/Player.gd
@@ -3,7 +3,7 @@ extends RigidBody2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-var speed = 0
+var speed = 500
 
 func _ready():
 	# Called every time the node is added to the scene.
