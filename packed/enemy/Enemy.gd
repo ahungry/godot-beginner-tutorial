b478561bd49589f66b5557b88d9b0cf82f981c26
Matthew Carter <m@ahungry.com>
2018-06-16 23:59:23 -0400

    More work on tut 5

--- packed/enemy/Enemy.gd
+++ packed/enemy/Enemy.gd
@@ -1,12 +1,16 @@
 extends RigidBody2D
 
 var _player = null
+var _speed_factor = 0.5
 
 func _ready():
 	_player = get_node("../Player")
 	set_process(true)
 
 func _process(delta):
-	var current_pos = get_pos()
-	current_pos.y = current_pos.y + _player.speed * delta
-	set_pos(current_pos)
+  # Math way
+	# var current_pos = get_pos()
+	# current_pos.y = current_pos.y + _player.speed * delta
+	# set_pos(current_pos)
+  # Vector way
+  set_pos(get_pos() + Vector2(0, _player.speed * _speed_factor * delta))
--- packed/player/Player.gd
+++ packed/player/Player.gd
@@ -5,6 +5,7 @@ var _max_speed = 500
 var _acceleration = 0
 
 func _ready():
+  connect("body_enter", self, "_on_body_enter")
   set_process(true)
 
 func _process(delta):
@@ -12,3 +13,6 @@ func _process(delta):
   speed = min(speed, _max_speed)
   if (speed < _max_speed):
       _acceleration += delta
+
+func _on_body_enter(other):
+  print(other.get_name())
